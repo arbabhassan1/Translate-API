ed28fee103752a21a4b8f59e7ba041aa6219d16e
arbabhassan1 <linkto898@hotmail.com>
2024-05-31 03:52:02 -0700

    initial Version

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 import express from "express";
-
+import translate from "translate";
+import cors from "cors";
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+translate.engine = "google";
+app.post("/translate", async (req, res) => {
+  const { content, language } = req.body;
 
-app.get("/translate", async (req, res) => {
-  res.json({
-    message: "OK",
-  });
+  if (!language) {
+    return res.status(400).json({
+      error: true,
+      message: "Language is required.",
+    });
+  }
+
+  if (!content) {
+    return res.status(400).json({
+      error: true,
+      message: "Content is required.",
+    });
+  }
+
+  try {
+    const text = await translate(content, { to: language });
+    console.log(text);
+    return res.json({
+      error: false,
+      data: text,
+    });
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
 });
 
 // Handle 404 errors
 app.use((req, res, next) => {
-  res.status(404).json({ error: "API route not found" });
+  res.status(404).json({ error: true, message: "API route not found" });
 });
 
 const PORT_NO = 8686;
